01a12bda8a45c31e775b6fbf0dd3b6bc4df86f5c
HuyyTran <huytrn.dev@gmail.com>
2024-02-07 23:53:10 +0700

    update mongDB

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
